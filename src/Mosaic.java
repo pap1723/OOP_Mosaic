02f200eb8797efa91c6439748f5d70594ce5e61e
Dan Paplaczyk <papwr17@gmail.com>
2018-04-18 11:10:21 -0500

    Reorganize Folder Structure

---
+++ src/Mosaic.java
@@ -0,0 +1,26 @@
+/**
+ * Mosaic Tile Program
+ * 
+ * <p>Mosaic is a GUI program that will create a tiled window that has randomly generated
+ * circles and squares. These shapes are filled with a random color and a random
+ * letter is placed over the top of the shape.
+ * 
+ * <p>There is some interaction that the user can have with the program. There is a button
+ * that will randomize all of the tiles and another button will randomize only one single
+ * tile. The user can also change the layout of the tiles. The default layout is a grid
+ * of 12 x 12, but the user can set any number of rows and columns in the input boxes.
+ * @author Dan Paplaczyk
+ * @version 1.0
+ *
+ */
+
+
+public class Mosaic {
+
+	public static void main(String[] args) {
+
+		TileFrame myFrame = new TileFrame();			// Calls the frame to be created
+
+	}
+
+}
